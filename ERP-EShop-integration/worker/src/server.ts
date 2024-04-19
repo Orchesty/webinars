c772275112da1f553c4dfe5174ba452a36631665
Radek Jirsa <jirsa.r@ghanaboso.com>
2024-04-19 09:08:12 +0200

    Deps updated

--- ERP-EShop-integration/worker/src/server.ts
+++ ERP-EShop-integration/worker/src/server.ts
@@ -3,7 +3,9 @@ import logger from '@orchesty/nodejs-sdk/dist/lib/Logger/Logger';
 import prepare from './index';
 
 // Start App by:
-prepare().then(listen).catch((e) => {
-    logger.error(e?.message, {});
-    process.exit(1);
-});
+prepare()
+    .then(listen)
+    .catch((e: unknown) => {
+        logger.error((e as { message?: string })?.message ?? '', {});
+        process.exit(1);
+    });
